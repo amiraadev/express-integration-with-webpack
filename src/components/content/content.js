65d370ba08daea74a033ce1d427c45810e30e5ad
amiraallagui <allagui_fawzi@outlook.fr>
2023-11-22 16:58:43 +0100

    first commit

---
+++ src/components/content/content.js
@@ -0,0 +1,15 @@
+import "./content.css"
+import image from "./ai_image.jpg"
+class Content {
+    render(){
+        const img = document.createElement("img");
+        img.src=image;
+        img.alt="couldn't find the image";
+        const body = document.querySelector("body")
+        const paragraph = document.createElement("p");
+        paragraph.innerHTML = "random paragraph";
+        body.appendChild(img);
+        body.appendChild(paragraph);
+    }
+}
+export default Content
