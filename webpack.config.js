274a7b21d1c2104709c287d72782866fc104ea18
amiraallagui <allagui_fawzi@outlook.fr>
2023-11-22 22:48:08 +0100

    adding html-webpack-plugin 1

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry:"./src/index.js",
