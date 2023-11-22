5c3071389eebdf13aee317e0fe896aa449f9a16f
amiraallagui <allagui_fawzi@outlook.fr>
2023-11-22 20:08:43 +0100

    mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry:"./src/index.js",
@@ -16,8 +17,11 @@ module.exports = {
             },
             {
                     test:/\.css$/,
-                    use:["style-loader","css-loader"]
+                    use:[MiniCssExtractPlugin.loader,"css-loader"]
             },
         ]
-    }
+    },
+    plugins:[
+        new MiniCssExtractPlugin({filename:"style.css"})
+    ]
 }
